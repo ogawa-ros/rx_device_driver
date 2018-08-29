d348b71b080a7fb7fecfbf5fbfdda7c42afc488b
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2018-08-29 17:37:48 +0000

    info: operation tested :thumbsup:

--- scripts/l218_temp_monitor.py
+++ scripts/l218_temp_monitor.py
@@ -17,7 +17,17 @@ class lakeshore218_driver(object):
         self.GPIB = GPIB
         self.com = pymeasure.gpib_prologix(self.IP, self.GPIB)
 
-    def measure(self, ch=1):
+    def measure(self):
+        self.com.open()
+        self.com.send('KRDG?')
+        raw = self.com.readline()
+        ret = raw.strip().split(',')
+        self.com.close()
+        temperature = list(map(float, ret))
+        
+        return temperature
+
+    def measure_ch(self, ch=1):
         self.com.open()
         self.com.send('KRDG? %d'%(ch))
         ret = self.com.readline()
@@ -26,25 +36,29 @@ class lakeshore218_driver(object):
 
         return temperature
 
+def str2list(param):
+    return param.strip('[').strip(']').split(',')
+    
 if __name__ == '__main__':
     node_name = 'lakeshore_218'
+    rospy.init_node(node_name)
+
     host = rospy.get_param('~host')
     port = rospy.get_param('~port')
     rate = rospy.get_param('~rate')
-
-    topic_name1 = rospy.get_param('~topic_name1')
-    topic_name2 = rospy.get_param('~topic_name2')
-    topic_name3 = rospy.get_param('~topic_name3')
-    topic_name4 = rospy.get_param('~topic_name4')
-    topic_name5 = rospy.get_param('~topic_name5')
-    topic_name6 = rospy.get_param('~topic_name6')
-    topic_name7 = rospy.get_param('~topic_name7')
-    topic_name8 = rospy.get_param('~topic_name8')
-
-    rospy.init_node(node_name)
+    hoge = rospy.get_param('~hoge')
+    
+    topic_name1 = str2list(rospy.get_param('~topic_name1'))
+    topic_name2 = str2list(rospy.get_param('~topic_name2'))
+    topic_name3 = str2list(rospy.get_param('~topic_name3'))
+    topic_name4 = str2list(rospy.get_param('~topic_name4'))
+    topic_name5 = str2list(rospy.get_param('~topic_name5'))
+    topic_name6 = str2list(rospy.get_param('~topic_name6'))
+    topic_name7 = str2list(rospy.get_param('~topic_name7'))
+    topic_name8 = str2list(rospy.get_param('~topic_name8'))
 
     try:
-        temperature = lakeshore2_driver(host, port)
+        temp = lakeshore218_driver(host, port)
     except OSError as e:
         rospy.logerr("{e.strerror}. host={host}".format(**locals()))
         sys.exit()
@@ -59,60 +73,62 @@ if __name__ == '__main__':
     pub8 = rospy.Publisher(topic_name8[0], Float64, queue_size=1)
 
     while not rospy.is_shutdown():
-        if topic_name1[1] == 0: pass
-        elif topic_name1[1] == 1:
+
+        ret = temp.measure()
+        
+        if int(topic_name1[1]) == 0: pass
+        elif int(topic_name1[1]) == 1:
             msg1 = Float64()
-            msg1.lakeshore_218_ch1 = temp.measure(1)
-            pub.publish(msg1)
-            return
-
-        if topic_name2[1] == 0: pass
-        elif topic_name2[1] == 1:
+            msg1.data = ret[0]
+            # msg1.data = temp.measure_ch(1)
+            pub1.publish(msg1)
+        
+        if int(topic_name2[1]) == 0: pass
+        elif int(topic_name2[1]) == 1:
             msg2 = Float64()
-            msg2.lakeshore_218_ch2 = temp.measure(2)
-            pub.publish(msg2)
-            return
+            msg2.data = ret[1]
+            # msg2.data = temp.measure_ch(2)
+            pub2.publish(msg2)
 
-        if topic_name3[1] == 0: pass
-        elif topic_name3[1] == 1:
+        if int(topic_name3[1]) == 0: pass
+        elif int(topic_name3[1]) == 1:
             msg3 = Float64()
-            msg3.lakeshore_218_ch3 = temp.measure(3)
-            pub.publish(msg3)
-            return
+            msg3.data = ret[2]           
+            # msg3.data = temp.measure_ch(3)
+            pub3.publish(msg3)
 
-        if topic_name4[1] == 0: pass
-        elif topic_name4[1] == 1:
+        if int(topic_name4[1]) == 0: pass
+        elif int(topic_name4[1]) == 1:
             msg4 = Float64()
-            msg4.lakeshore_218_ch4 = temp.measure(4)
-            pub.publish(msg4)
-            return
+            msg4.data = ret[3]                       
+            # msg4.data = temp.measure_ch(4)
+            pub4.publish(msg4)
 
-        if topic_name5[1] == 0: pass
-        elif topic_name5[1] == 1:
+        if int(topic_name5[1]) == 0: pass
+        elif int(topic_name5[1]) == 1:
             msg5 = Float64()
-            msg5.lakeshore_218_ch5 = temp.measure(5)
-            pub.publish(msg5)
-            return
+            msg5.data = ret[4]                       
+            # msg5.data = temp.measure_ch(5)
+            pub5.publish(msg5)
 
-        if topic_name6[1] == 0: pass
-        elif topic_name1[1] == 1:
+        if int(topic_name6[1]) == 0: pass
+        elif int(topic_name6[1]) == 1:
             msg6 = Float64()
-            msg6.lakeshore_218_ch6 = temp.measure(6)
+            msg6.data = ret[5]                       
+            # msg6.data = temp.measure_ch(6)
             pub6.publish(msg6)
-            return
-
-        if topic_name7[1] == 0: pass
-        elif topic_name1[1] == 1:
+        
+        if int(topic_name7[1]) == 0: pass
+        elif int(topic_name1[1]) == 1:
             msg7 = Float64()
-            msg7.lakeshore_218_ch1 = temp.measure(1)
+            msg7.data = ret[7]                       
+            # msg7.data = temp.measure_ch(7)
             pub7.publish(msg7)
-            return
 
-        if topic_name8[1] == 0: pass
-        elif topic_name8[1] == 1:
+        if int(topic_name8[1]) == 0: pass
+        elif int(topic_name8[1]) == 1:
             msg8 = Float64()
-            msg8.lakeshore_218_ch8 = temp.measure(8)
-            pub.publish(msg8)
-            return
+            msg8.data = ret[7]                       
+            # msg8.data = temp.measure_ch(8)
+            pub8.publish(msg8)
 
-        continue
