72faceffdb7dae0f7ac5733fa4515a8d4d76c2ab
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2018-08-30 17:23:38 +0000

    clean: :sparkles:

--- scripts/l218_temp_monitor.py
+++ scripts/l218_temp_monitor.py
@@ -1,8 +1,6 @@
 #! /usr/bin/env python3
 
-import sys
 import time
-import threading
 import pymeasure
 
 import rospy
@@ -27,15 +25,7 @@ class lakeshore218_driver(object):
         
         return temperature
 
-    def measure_ch(self, ch=1):
-        self.com.open()
-        self.com.send('KRDG? %d'%(ch))
-        ret = self.com.readline()
-        self.com.close()
-        temperature = float(ret)
-
-        return temperature
-
+    
 def str2list(param):
     return param.strip('[').strip(']').split(',')
     
@@ -57,17 +47,15 @@ if __name__ == '__main__':
         rospy.logerr("{e.strerror}. host={host}".format(**locals()))
         sys.exit()
 
-    pub_list = [rospy.Publisher(topic_name[topic_name_index], Float64, queue_size=1) for topic_name in topic_list]
+    pub_list = [rospy.Publisher(topic[topic_name_index], Float64, queue_size=1) for topic in topic_list if int(topic[onoff_index]) == 1]
     msg_list = [Float64() for i in range(ch_number)]
     
     while not rospy.is_shutdown():
 
         ret = temp.measure()
 
-        for topic, pub, msg, i in zip(topic_list, pub_list, msg_list, range(8)):
-            if int(topic[onoff_index]) == 0: pass
-            elif int(topic[onoff_index]) == 1:
-                msg.data = ret[i]
-                pub.publish(msg)
-            continue
+        for pub, msg, i in zip(pub_list, msg_list, range(8)):
+            msg.data = ret[i]
+            pub.publish(msg)
         continue
+
