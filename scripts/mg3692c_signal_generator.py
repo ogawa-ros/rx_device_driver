6d5736b764141d52bfe7aab12cac431289629d31
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2019-01-15 15:26:48 +0900

    bugfix

---
+++ scripts/mg3692c_signal_generator.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+
+import rospy
+from std_msgs.msg import Int32
+from std_msgs.msg import Float64
+
+import sys
+import time
+import pymeasure
+
+
+node_name = 'mg3692c'
+
+
+class InvalidRangeError(Exception):
+    pass
+
