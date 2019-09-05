984c7b75cd7fefe8f87e78b92951f34aa2466f9b
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2019-09-05 22:26:49 +0900

    info : minor change

--- scripts/e8257d_signal_generator.py
+++ scripts/e8257d_signal_generator.py
@@ -22,30 +22,23 @@ class e8257d_driver(object):
     def __init__(self, IP='', GPIB=1):
         self.IP = IP
         self.GPIB = GPIB
+        self.com = pymeasure.gpib_prologix(self.IP, self.GPIB)
+        self.com.open()
 
 
     def set_freq(self, freq, unit='GHz'):
-        self.com = pymeasure.gpib_prologix(self.IP, self.GPIB)
-        self.com.open()
         self.com.send('FREQ:CW %.10f %s'%(freq, unit))
-        self.com.close()
         return
 
     def get_freq(self):
-        self.com = pymeasure.gpib_prologix(self.IP, self.GPIB)
-        self.com.open()
         self.com.send('FREQ:CW?')
         ret = self.com.readline()
-        self.com.close()
 
-        return float(ret)/1e+9
+        return float(ret) / 1e+9
 
     def set_power(self, power=-20.0):
-        self.com = pymeasure.gpib_prologix(self.IP, self.GPIB)
         if -20.0<=power<=30.0:
-            self.com.open()
             self.com.send('POW %f dBm'%(power))
-            self.com.close()
         else:
             msg = 'Power range is -20.0[dBm] -- 30.0[dBm],'
             msg += ' while {}[dBm] is given.'.format(power)
@@ -53,30 +46,21 @@ class e8257d_driver(object):
         return
 
     def get_power(self):
-        self.com = pymeasure.gpib_prologix(self.IP, self.GPIB)
-        self.com.open()
         self.com.send('POW?')
         ret = self.com.readline()
-        self.com.close()
 
         return float(ret)
 
     def set_onoff(self, onoff=0):
-        self.com = pymeasure.gpib_prologix(self.IP, self.GPIB)
-        self.com.open()
         if onoff==1:
             self.com.send('OUTP ON')
         else:
             self.com.send('OUTP OFF')
-        self.com.close()
         return
 
     def get_onoff(self):
-        self.com = pymeasure.gpib_prologix(self.IP, self.GPIB)
-        self.com.open()
         self.com.send('OUTP?')
         ret = self.com.readline()
-        self.com.close()
 
         return int(ret)
 
