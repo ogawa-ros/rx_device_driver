81367aa48a566f82e858204ba101968399085a53
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2019-09-05 23:08:26 +0900

    info : SG 関連モジュール終了

--- scripts/mg3692c_signal_generator.py
+++ scripts/mg3692c_signal_generator.py
@@ -22,30 +22,28 @@ class mg3692c_driver(object):
     def __init__(self, IP='', GPIB=1):
         self.IP = IP
         self.GPIB = GPIB
+        self.com = pymeasure.gpib_prologix(self.IP, self.GPIB)
 
     def set_freq(self, freq, unit='GHz'):
-        self.com = pymeasure.gpib_prologix(IP, GPIB)
-        self.com.open()
+        self.open()
         self.com.send('FREQ:CW %.10f %s'%(freq, unit))
-        self.com.close()
+        self.close()
         return
 
     def query_freq(self):
-        self.com = pymeasure.gpib_prologix(IP, GPIB)
-        self.com.open()
+        self.open()
         self.com.send('FREQ:CW?')
         ret = self.com.readline()
         self.com.close()
-        freq = float(ret)/1e+9
+        freq = float(ret) / 1e+9
 
         return freq
 
     def set_power(self, power=-20.0):
-        self.com = pymeasure.gpib_prologix(IP, GPIB)
         if -20.0<=power<=30.0:
-            self.com.open()
+            self.open()
             self.com.send('POW %f dBm'%(power))
-            self.com.close()
+            self.close()
         else:
             msg = 'Power range is -20.0[dBm] -- 30.0[dBm],'
             msg += ' while {}[dBm] is given.'.format(power)
@@ -53,17 +51,15 @@ class mg3692c_driver(object):
         return
 
     def query_power(self):
-        self.com = pymeasure.gpib_prologix(IP, GPIB)
-        self.com.open()
+        self.open()
         self.com.send('POW?')
         ret = self.com.readline()
-        self.com.close()
+        self.close()
         power = float(ret)
 
         return power
 
     def set_output(self, onoff=0):
-        self.com = pymeasure.gpib_prologix(IP, GPIB)
         self.com.open()
         if onoff==1:
             self.com.send('OUTP ON')
@@ -73,7 +69,6 @@ class mg3692c_driver(object):
         return
 
     def query_output(self):
-        self.com = pymeasure.gpib_prologix(IP, GPIB)
         self.com.open()
         self.com.send('OUTP?')
         ret = self.com.readline()
@@ -88,37 +83,68 @@ class mg3692c_controller(object):
     def __init__(self):
         host = rospy.get_param('~host')
         port = rospy.get_param('~port')
-        self.sg_name = rospy.get_param('~sg_name')
         self.sg = mg3692c_driver(host, port)
 
-        self.topic_list = ['freq', 'power', 'onoff']
-        self.data_class_list = [Float64, Float64, Int32]
-        self.pub_list = [
-            rospy.Publisher(
-                name = '{0}_{1}'.format(self.sg_name, topic),
-                data_class = _data_class,
-                latch = True,
-                queue_size = 1
-            )
-            for topic, _data_class in zip(self.topic_list, self.data_class_list)
-        ]
-        self.sub_list = [
-            rospy.Subscriber(
-                name = '{0}_{1}_cmd'.format(self.sg_name, topic),
-                data_class = _data_class,
-                callback = self.callback,
-                callback_args = topic,
-                queue_size = 1
-            )
-            for topic, _data_class in zip(self.topic_list, self.data_class_list)
-        ]
+    self.pub_freq = rospy.Publisher(
+        name = '{}_freq'.format(self.sg_name),
+        data_class = Float64,
+        latch = True,
+        queue_size = 1
+        )
+    self.pub_power = rospy.Publisher(
+        name = '{}_power'.format(self.sg_name),
+        data_class = Float64,
+        latch = True,
+        queue_size = 1
+        )
+    self.pub_freq = rospy.Publisher(
+        name = '{}_onoff'.format(self.sg_name),
+        data_class = Int32,
+        latch = True,
+        queue_size = 1
+        )
 
+    self.sub_freq = rospy.Subscriber(
+        name = '{}_freq_cmd'.format(self.sg_name),
+        data_class = Float64,
+        callback = self.callback_freq,
+        queue_size = 1
+        )
+    self.sub_power = rospy.Subscriber(
+        name = '{}_power_cmd'.format(self.sg_name),
+        data_class = Float64,
+        callback = self.callback_power,
+        queue_size = 1
+        )
+    self.sub_onoff = rospy.Subscriber(
+        name = '{}_onoff_cmd'.format(self.sg_name),
+        data_class = Float64,
+        callback = self.callback_onoff,
+        queue_size = 1
+        )
 
-    def callback(self, q, topic):
+    def callback_freq(self, q):
         target = q.data
-        exec('self.sg.set_{}(target)'.format(topic))
-        current = exec('self.sg.get_{}()'.format(topic))
-        self.pub_list[topic_list.index(topic)].publish(current)
+        self.sg.set_freq(target)
+        time.sleep(1)
+        current = self.sg.get_freq()
+        self.pub_freq.publish(current)
+        return
+
+    def callback_power(self, q):
+        target = q.data
+        self.sg.set_power(target)
+        time.sleep(1)
+        current = self.sg.get_power()
+        self.pub_power.publish(current)
+        return
+
+    def callback_onoff(self, q):
+        target = q.data
+        self.sg.set_onoff(target)
+        time.sleep(1)
+        current = self.sg.get_onoff()
+        self.pub_onoff.publish(current)
         return
 
 
