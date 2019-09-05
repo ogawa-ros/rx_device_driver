17956f37ce53cd5da5a8749df8ea144bcbc4c634
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2019-09-05 18:15:14 +0900

    info : debug

--- scripts/a11713b_patt_driver.py
+++ scripts/a11713b_patt_driver.py
@@ -100,7 +100,7 @@ class a11713b_controller(object):
         level = q.data
         self.driver.set_level(level=level, ch=ch)
         current = self.driver.query_level()[self.ch_list.index(ch)]
-        self.pub_list[self.ch_list.index(ch)].publish(cuurent)
+        self.pub_list[self.ch_list.index(ch)].publish(current)
         return
 
 if __name__ == '__main__':
